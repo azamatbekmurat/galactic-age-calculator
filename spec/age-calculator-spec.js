b110a0c5ab4598436303378a788d6384cde6f6c1
Azamat <azamat.bekmuratov24@gmail.com>
2018-05-25 16:22:24 -0700

    add functions to calculate life expactancy on each planet, and calculate the number of years after life expectancy has been passed

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -17,31 +17,53 @@ describe ('age-calculator', function() {
   it ('should test difference in seconds between two dates', function() {
     var newAge = new AgeCalculator();
     expect(newAge.difference("05/23/2018", "05/24/2018")).toEqual(86400);
-    console.log("difference in seconds: " + newAge.difference("05/23/2018", "05/24/2018"));
   });
 
   it ('should test the converstion of Earth year to Mercury year', function() {
     var newAge = new AgeCalculator();
     expect(newAge.humanYearToMerc(12)).toEqual(50);
-    console.log("Mercury year is: " + newAge.humanYearToMerc(12));
   });
 
   it ('should test the converstion of Earth year to Venus year', function() {
     var newAge = new AgeCalculator();
     expect(newAge.humanYearToVenus(12)).toEqual(19.35);
-    console.log("Venus year is: " + newAge.humanYearToVenus(12));
   });
 
   it ('should test the converstion of Earth year to Mars year', function() {
     var newAge = new AgeCalculator();
     expect(newAge.humanYearToMars(12)).toEqual(6.38);
-    console.log("Mars year is: " + newAge.humanYearToMars(12));
   });
 
   it ('should test the converstion of Earth year to Jupiter year', function() {
     var newAge = new AgeCalculator();
     expect(newAge.humanYearToJupiter(12)).toEqual(1.01);
-    console.log("Jupiter year is: " + newAge.humanYearToJupiter(12));
+  });
+
+  it ('should test the life expactancy of each planet when life expactancy on Earth is 80', function() {
+    var newAge = new AgeCalculator();
+    expect(newAge.lifeExpactancy("Earth")).toEqual(80);
+    expect(newAge.lifeExpactancy("Mercury")).toEqual(333.33);
+    expect(newAge.lifeExpactancy("Venus")).toEqual(129.03);
+    expect(newAge.lifeExpactancy("MARS")).toEqual(42.55);
+    expect(newAge.lifeExpactancy("juPITER")).toEqual(6.75);
+  });
+
+  it ('should test the converstion of Earth years to particular planet', function() {
+    var newAge = new AgeCalculator();
+    expect(newAge.earthYearsToPlanet(30, "Earth")).toEqual(30);
+    expect(newAge.earthYearsToPlanet(30, "Mercury")).toEqual(125);
+    expect(newAge.earthYearsToPlanet(30, "Jupiter")).toEqual(2.53);
+  });
+
+
+  it ('should test how many years have been passed on each particular planet after life expactancy', function() {
+    var newAge = new AgeCalculator();
+    expect(newAge.yearAfterLifeExpactancy(78, "mercury")).not.toEqual(2);
+    expect(newAge.yearAfterLifeExpactancy(82, "Earth")).toEqual(2);
+    expect(newAge.yearAfterLifeExpactancy(82, "Mercury")).toEqual(8.34);
+    expect(newAge.yearAfterLifeExpactancy(82, "Venus")).toEqual(3.23);
+    expect(newAge.yearAfterLifeExpactancy(82, "Mars")).toEqual(1.07);
+    expect(newAge.yearAfterLifeExpactancy(82, "Jupiter")).toEqual(0.16);
   });
 
 
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -40,6 +40,56 @@ export class AgeCalculator {
     return jupiterYear;
   }
 
+  lifeExpactancy(planet) {
+    let planetInput = planet.toLowerCase();
+    let EarthLifeExpactancy = 80;
+    let planetLifeExpactancy;
+
+    if (planetInput == "earth") {
+      planetLifeExpactancy = 80;
+    } else if (planetInput == "mercury") {
+      planetLifeExpactancy = this.humanYearToMerc(EarthLifeExpactancy);
+    } else if (planetInput == "venus") {
+      planetLifeExpactancy = this.humanYearToVenus(EarthLifeExpactancy);
+    } else if (planetInput == "mars") {
+      planetLifeExpactancy = this.humanYearToMars(EarthLifeExpactancy);
+    } else if (planetInput == "jupiter") {
+      planetLifeExpactancy = this.humanYearToJupiter(EarthLifeExpactancy);
+    }
+    return planetLifeExpactancy;
+  }
+
+  earthYearsToPlanet(EarthYears, planet) {
+    let planetInput = planet.toLowerCase();
+    let planetYears = 0;
+
+    if (planetInput == "earth") {
+      planetYears = EarthYears;
+    } else if (planetInput == "mercury") {
+      planetYears = this.humanYearToMerc(EarthYears);
+    } else if (planetInput == "venus") {
+      planetYears = this.humanYearToVenus(EarthYears);
+    } else if (planetInput == "mars") {
+      planetYears = this.humanYearToMars(EarthYears);
+    } else if (planetInput == "jupiter") {
+      planetYears = this.humanYearToJupiter(EarthYears);
+    }
+
+    return planetYears;
+  }
+
+  yearAfterLifeExpactancy(livingEarthYears, planet) {
+    let planetInput = planet.toLowerCase();
+    let averageLifeExpactancyYears = this.lifeExpactancy(planetInput);
+    let actualPlanetAge = this.earthYearsToPlanet(livingEarthYears, planetInput);
+    let afterLifeExpactancy;
+
+    if (actualPlanetAge > averageLifeExpactancyYears) {
+      afterLifeExpactancy = this.roundToTwo(Math.abs(actualPlanetAge - averageLifeExpactancyYears));
+    }
+    return afterLifeExpactancy;
+  }
+
   roundToTwo(num) {
     return +(Math.round(num + "e+2")  + "e-2");
   }
